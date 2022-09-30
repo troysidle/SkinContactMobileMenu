8b9d977595c6f860baa9a9f876d397dbceb281f4
Troy Sidle <troysidle@gmail.com>
2022-09-29 23:55:25 -0400

    While Loop with Array
    ... work in progress

---
+++ While.py
@@ -0,0 +1,11 @@
+#   Work in Progress for Converting Skin Contact published .pdf file to Bootstrap, Mobile friendly version.
+#   Troy Sidle
+#   copyright 2022
+
+sampleText = "stop"
+transportation = ["busses", "trains", "stop", "bikes"]
+
+i = 1
+while i < 6:
+  print(i)
+  i += 1
