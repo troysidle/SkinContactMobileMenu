88561784a5f462bd5afa30bc076bf4256debf00d
Troy Sidle <troysidle@gmail.com>
2022-09-30 15:08:40 -0400

    Debugging in Process
    sample code works, but I suspect I have an off by 1 error.

--- While.py
+++ While.py
@@ -1,11 +1,54 @@
-#   Work in Progress for Converting Skin Contact published .pdf file to Bootstrap, Mobile friendly version.
-#   Troy Sidle
-#   copyright 2022
+#This code produces an error, but otherwise works as designed. Currently debugging.
 
-sampleText = "stop"
-transportation = ["busses", "trains", "stop", "bikes"]
+templateHeader = []
+with open('Template.txt') as templateFile:
+    templateHeader = templateFile.readlines()
 
-i = 1
-while i < 6:
-  print(i)
-  i += 1
+
+
+
+cars = ['Ford', 'BMW', 'Volvo', 'stop', 'Audi']
+keyWord = 'stop\n'
+matched_indexes = [] #rename this variable
+i = 0
+templateLength = len(templateHeader)
+
+while i < templateLength and keyWord !=templateHeader[i]:
+    matched_indexes.append(i)
+    print(templateHeader[i])
+    i += 1
+
+print('Count is', i)
+    
+contentByTheGlass = []
+with open('Content.txt') as contentFile:
+    contentByTheGlass = contentFile.readlines()    
+    
+contentKeyWord = 'FOOD\n'
+j = 0
+templateLength = len(contentByTheGlass)
+
+while j < templateLength and keyWord !=contentByTheGlass[j]:
+
+    print(contentByTheGlass[j])
+    j += 1
+    
+    
+print('Count is', i)  
+#i += 1
+#print('Count is', i)
+
+
+while i < templateLength: # and keyWord !=templateHeader[i]:
+    matched_indexes.append(i)
+    print(templateHeader[i])
+    print('Count is', i)
+    i += 1    
+
+print('Count is', i)    
+    
+    
+print(f'{keyWord} is not present in {templateHeader} at indexes {matched_indexes}')
+
+
+  
