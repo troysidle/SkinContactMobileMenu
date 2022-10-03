e9cf2d9cf349f5a9a31a95f1e23c7ccd37528b96
Troy Sidle <troysidle@gmail.com>
2022-10-03 15:36:53 -0400

    Updated with better logic
    Still in progress.

--- While.py
+++ While.py
@@ -1,54 +1,69 @@
 #This code produces an error, but otherwise works as designed. Currently debugging.
 
-templateHeader = []
-with open('Template.txt') as templateFile:
-    templateHeader = templateFile.readlines()
+#This code reads in two documents as arrays and creates a third file that combines them.
 
 
+#designate the array for the HTML code.
+templateHeader = ['<HTML>', 'line1', 'line2', '<p>', 'stop', '</p>', '<!--end-->', '</HTML>']
+#with open('Template.txt') as templateFile:
+#    templateHeader = templateFile.readlines()
 
 
-cars = ['Ford', 'BMW', 'Volvo', 'stop', 'Audi']
-keyWord = 'stop\n'
-matched_indexes = [] #rename this variable
+#Define the key word to look for within the HTML file.
+keyWord = 'stop'
+
+#define an array to show what is happening at the end.
+resultContent = [] 
+
 i = 0
 templateLength = len(templateHeader)
 
+#Print all the elements of the array until the keyword is reached.
+#Add those elements to the results array.
 while i < templateLength and keyWord !=templateHeader[i]:
-    matched_indexes.append(i)
+    resultContent.append(i)
     print(templateHeader[i])
+    #print('Count is', i)
     i += 1
 
-print('Count is', i)
+print('    ')
+#print('		After loop, count is', i)
     
-contentByTheGlass = []
-with open('Content.txt') as contentFile:
-    contentByTheGlass = contentFile.readlines()    
-    
-contentKeyWord = 'FOOD\n'
-j = 0
-templateLength = len(contentByTheGlass)
+contentByTheGlass = ['FOOD', 'Almonds', 'Pickles', 'Cheese']
+#with open('Content.txt') as contentFile:
+#    contentByTheGlass = contentFile.readlines()    
 
-while j < templateLength and keyWord !=contentByTheGlass[j]:
+#not yet using contentKeyWord
+contentKeyWord = 'FOOD'
+j = 0
+contentLength = len(contentByTheGlass)
+
+while j < contentLength:
 
     print(contentByTheGlass[j])
+    resultContent.append(i+j)
     j += 1
     
     
-print('Count is', i)  
-#i += 1
-#print('Count is', i)
+print('    ')
+    
+#print('			J count is', j)  
+i += 1
+#print('		Count is', i)
 
 
 while i < templateLength: # and keyWord !=templateHeader[i]:
-    matched_indexes.append(i)
+    #matched_indexes.append(i)
     print(templateHeader[i])
-    print('Count is', i)
+    #print('		Count is', i)
     i += 1    
 
-print('Count is', i)    
+
+print('    ')
+print('I count is', i)    
     
     
-print(f'{keyWord} is not present in {templateHeader} at indexes {matched_indexes}')
+print(f'{keyWord} is not present in {templateHeader} at indexes {resultContent}')
 
 
   
